f95ce93013386deb3b52c5e23226cae526a87848
Janak <janak2@users.noreply.github.com>
2024-06-18 13:30:06 -0700

    Update __init__.py (#4)

--- realtime/web_endpoint/__init__.py
+++ realtime/web_endpoint/__init__.py
@@ -3,6 +3,7 @@ import functools
 import inspect
 import logging
 import os
+import ssl
 import time
 
 import uvicorn
@@ -23,7 +24,18 @@ def web_endpoint(method: str = "POST", path: str = "/"):
 
             HOSTNAME = "0.0.0.0"
             PORT = int(os.getenv("HTTP_PORT", 8080))
-            server = uvicorn.Server(config=uvicorn.Config(webrtc_app, host=HOSTNAME, port=PORT, log_level="info"))
+            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            ssl_context.load_cert_chain(os.environ["SSL_CERT_PATH"], keyfile=os.environ["SSL_KEY_PATH"])
+            server = uvicorn.Server(
+                config=uvicorn.Config(
+                    webrtc_app,
+                    host=HOSTNAME,
+                    port=PORT,
+                    log_level="info",
+                    ssl_keyfile=os.environ["SSL_KEY_PATH"],
+                    ssl_certfile=os.environ["SSL_CERT_PATH"],
+                )
+            )
             await server.serve()
 
         return wrapper
