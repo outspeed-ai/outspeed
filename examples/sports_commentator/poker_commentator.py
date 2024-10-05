996580becdfb11a37265e6b7764dd21caf013b68
janak2 <janakagrawal1309@gmail.com>
2024-10-04 18:46:35 -0700

    Update poker_commentator.py

--- examples/sports_commentator/poker_commentator.py
+++ examples/sports_commentator/poker_commentator.py
@@ -11,7 +11,7 @@ import outspeed as sp
 class PokerCommentator:
     async def setup(self):
         self.deepgram_node = sp.DeepgramSTT(sample_rate=8000)
-        self.keyframe_node = sp.KeyFrameDetector(key_frame_threshold=0.8, key_frame_max_time=15)
+        self.keyframe_node = sp.KeyFrameDetector(key_frame_threshold=0.9, key_frame_max_time=20)
         self.llm_node = sp.GeminiVision(
             system_prompt="You are a poker commentator. Your job is to provide useful and deep insights on the strategy. Use exclamation points to show excitement. Do not mention the pot size. Make sure to read the cards correctly. If no cards are shown then just say something to pass time. Keep the response short and concise.",
             temperature=0.9,
@@ -21,10 +21,12 @@ class PokerCommentator:
         )
         self.token_aggregator_node = sp.TokenAggregator()
         self.tts_node = sp.CartesiaTTS(voice_id="5619d38c-cf51-4d8e-9575-48f61a280413")
+        self.vad_node = sp.SileroVAD(sample_rate=8000, min_volume=0)
 
     @sp.streaming_endpoint()
     async def run(self, audio_input_stream: sp.AudioStream, video_input_stream: sp.VideoStream):
         deepgram_stream: sp.TextStream = self.deepgram_node.run(audio_input_stream)
+        vad_stream: sp.VADStream = self.vad_node.run(audio_input_stream.clone())
 
         key_frame_stream: sp.VideoStream = self.keyframe_node.run(video_input_stream)
 
@@ -37,6 +39,11 @@ class PokerCommentator:
         token_aggregator_stream: sp.TextStream = self.token_aggregator_node.run(llm_token_stream)
 
         tts_stream: sp.AudioStream = self.tts_node.run(token_aggregator_stream)
+
+        self.llm_node.set_interrupt_stream(vad_stream)
+        self.token_aggregator_node.set_interrupt_stream(vad_stream.clone())
+        self.tts_node.set_interrupt_stream(vad_stream.clone())
+        self.keyframe_node.set_interrupt_stream(vad_stream.clone())
 
         return tts_stream, chat_history_stream
 
