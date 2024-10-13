1c2092a1c7d5c73fd51aab4bf39bb4228c6f9e9f
janak2 <janakagrawal1309@gmail.com>
2024-10-13 10:15:56 -0700

    version to 0.1.151

--- examples/openai_realtime/rag.py
+++ examples/openai_realtime/rag.py
@@ -18,7 +18,7 @@ def check_outspeed_version():
 
     from packaging import version
 
-    required_version = "0.1.149"
+    required_version = "0.1.151"
 
     try:
         current_version = importlib.metadata.version("outspeed")
