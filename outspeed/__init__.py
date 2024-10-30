134c0cc6da6a6e388c107008855d38582d5f6261
janak2 <janakagrawal1309@gmail.com>
2024-10-30 00:33:48 -0700

    add custom llm node

--- outspeed/__init__.py
+++ outspeed/__init__.py
@@ -4,8 +4,7 @@ import sys
 if sys.version_info[:2] < (3, 9):
     raise RuntimeError("This version of Outspeed requires at least Python 3.9")
 if sys.version_info[:2] >= (3, 13):
-    raise RuntimeError(
-        "This version of Outspeed does not support Python 3.13+")
+    raise RuntimeError("This version of Outspeed does not support Python 3.13+")
 
 
 import logging
@@ -22,23 +21,23 @@ import coloredlogs
 
 
 def cross_platform_where():
-    if platform.system() == 'Darwin':  # macOS
-        if os.path.exists('/etc/ssl/cert.pem'):
-            return '/etc/ssl/cert.pem'
+    if platform.system() == "Darwin":  # macOS
+        if os.path.exists("/etc/ssl/cert.pem"):
+            return "/etc/ssl/cert.pem"
         else:
             return certifi.where()
-    elif os.name == 'posix':  # Linux and other POSIX systems
-        if os.path.exists('/etc/ssl/certs/ca-certificates.crt'):
-            return '/etc/ssl/certs/ca-certificates.crt'
-        elif os.path.exists('/etc/ssl/cert.pem'):
-            return '/etc/ssl/cert.pem'
+    elif os.name == "posix":  # Linux and other POSIX systems
+        if os.path.exists("/etc/ssl/certs/ca-certificates.crt"):
+            return "/etc/ssl/certs/ca-certificates.crt"
+        elif os.path.exists("/etc/ssl/cert.pem"):
+            return "/etc/ssl/cert.pem"
         else:
             return certifi.where()
     else:  # Windows and others
         return certifi.where()
 
 
-os.environ['SSL_CERT_FILE'] = cross_platform_where()
+os.environ["SSL_CERT_FILE"] = cross_platform_where()
 
 
 def configure_logging(level=logging.INFO):
@@ -52,8 +51,7 @@ def configure_logging(level=logging.INFO):
     # By default the install() function installs a handler on the root logger,
     # this means that log messages from your code and log messages from the
     # libraries that you use will all show up on the terminal.
-    coloredlogs.install(
-        level=level, fmt="%(asctime)s %(levelname)s %(message)s")
+    coloredlogs.install(level=level, fmt="%(asctime)s %(levelname)s %(message)s")
 
     # If you don't want to see log messages from libraries, you can pass a
 
@@ -93,6 +91,7 @@ try:
     from .utils.vad import VADState  # noqa: F401
     from .web_endpoint import web_endpoint  # noqa: F401
     from .websocket import websocket  # noqa: F401
+    from .nodes import CustomLLMNode  # noqa: F401
 except Exception:
     print()
     print("#" * 50)
@@ -138,4 +137,5 @@ __all__ = [
     "Tool",
     "RealtimeServer",
     "AzureTranscriber",
+    "CustomLLMNode",
 ]
